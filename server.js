8a008f23f33e4837895d9c947bba774466d46553
alejandrorcld <alejandrorcld@gmail.com>
2025-11-19 23:16:40 -0300

    Refactor server.js for improved API response handling

--- server.js
+++ server.js
@@ -1,29 +1,16 @@
-const express = require('express');
-const cors = require('cors');
-
-const app = express();
-
-// Desactiva ETag y cache
-app.set('etag', false);
-app.use(cors({ optionsSuccessStatus: 200 }));
-app.use((req, res, next) => {
-  res.set({
-    'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate',
-    'Pragma': 'no-cache',
-    'Expires': '0',
-    'Surrogate-Control': 'no-store'
+app.get('/api', (req, res) => {
+  const date = new Date();
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
   });
-  next();
 });
 
-app.get('/api/:date?', (req, res) => {
+app.get('/api/:date', (req, res) => {
   const dateParam = req.params.date;
   let date;
 
-  if (!dateParam) {
-    // Fecha actual calculada en el momento exacto del request
-    date = new Date();
-  } else if (!isNaN(dateParam)) {
+  if (!isNaN(dateParam)) {
     date = new Date(parseInt(dateParam, 10));
   } else {
     date = new Date(dateParam);
@@ -33,11 +20,8 @@ app.get('/api/:date?', (req, res) => {
     return res.json({ error: 'Invalid Date' });
   }
 
-  return res.json({
+  res.json({
     unix: date.getTime(),
     utc: date.toUTCString()
   });
 });
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT);
